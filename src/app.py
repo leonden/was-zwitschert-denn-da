ae5624cda6aab8c76b504d55d56800b8073de0e0
Leon <leon.hochwimmer@breitband.ch>
2024-10-19 16:16:24 +0200

    Update

--- src/app.py
+++ src/app.py
@@ -1,37 +1,40 @@
-#111!/usr/bin/env python
-# -*- coding: utf-8 -*
-
-#sudo apt-get install python3-flask
-#pip3 install opencv-python
-
 from flask import Flask, render_template, Response
 import cv2
 
 app = Flask(__name__)
-#app.config["CACHE_TYPE"] = "null"
 
 @app.route('/')
 def index():
     """Video streaming home page."""
     return render_template('index.html')
 
-def gen():
+def gen(camera_index=0):
     """Video streaming generator function."""
-    vs = cv2.VideoCapture(0)
+    vs = cv2.VideoCapture(camera_index)  # Try different camera index here if 0 doesn't work.
+    if not vs.isOpened():
+        print(f"Cannot open camera {camera_index}")
+        return
+
     while True:
-        ret,frame=vs.read()
+        ret, frame = vs.read()
+        if not ret:
+            print("Failed to grab frame")
+            break
         ret, jpeg = cv2.imencode('.jpg', frame)
-        frame=jpeg.tobytes()
+        if not ret:
+            print("Failed to encode frame")
+            break
+        frame = jpeg.tobytes()
         yield (b'--frame\r\n'
-        b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-        
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
     vs.release()
-    cv2.destroyAllWindows() 
+    cv2.destroyAllWindows()
 
 @app.route('/video_feed')
 def video_feed():
-    """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(),mimetype='multipart/x-mixed-replace; boundary=frame')
+    """Video streaming route."""
+    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
