f10d810fc49d375e4468b755bffb67dcba9b38b6
Leon <leon.hochwimmer@breitband.ch>
2024-10-22 14:34:40 +0200

    Update

--- src/app.py
+++ src/app.py
@@ -1,15 +1,14 @@
+#111!/usr/bin/env python
+# -*- coding: utf-8 -*
+
+#sudo apt-get install python3-flask
+#pip3 install opencv-python
+
 from flask import Flask, render_template, Response
-from picamera2 import Picamera2
-from io import BytesIO
-from PIL import Image
+import cv2
 
 app = Flask(__name__)
-
-# Initialize the camera
-picam2 = Picamera2()
-# Configure the camera to stream in a suitable format
-picam2.configure(picam2.create_preview_configuration(main={"format": "RGB888", "size": (640, 480)}))
-picam2.start()
+#app.config["CACHE_TYPE"] = "null"
 
 @app.route('/')
 def index():
@@ -17,25 +16,22 @@ def index():
     return render_template('index.html')
 
 def gen():
-    """Video streaming generator function using picamera2."""
+    """Video streaming generator function."""
+    vs = cv2.VideoCapture(0)
     while True:
-        # Capture an image from the camera
-        frame = picam2.capture_array()
-
-        # Convert the frame to JPEG using PIL
-        image = Image.fromarray(frame)
-        buffer = BytesIO()
-        image.save(buffer, 'JPEG')
-        frame = buffer.getvalue()
-
-        # Yield the frame as part of an HTTP multipart response
+        ret,frame=vs.read()
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        frame=jpeg.tobytes()
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+    vs.release()
+    cv2.destroyAllWindows() 
 
 @app.route('/video_feed')
 def video_feed():
-    """Video streaming route."""
-    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(),mimetype='multipart/x-mixed-replace; boundary=frame')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
+if __name__ == '__main__': 
+    app.run(host='0.0.0.0', port =80, debug=True, threaded=True)
